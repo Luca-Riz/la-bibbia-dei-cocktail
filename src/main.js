b2d78b713f418ab86d6de8975a61a39b145fe2d2
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-07-30 19:49:44 +0200

    install dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@fortawesome/fontawesome-free/js/all.min.js';
+//import 'bootstrap/dist/js/bootstrap.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false
 
