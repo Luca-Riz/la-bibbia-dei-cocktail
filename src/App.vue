918bf041193c60ee7e86ce71eac67d0655cd7735
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-07-31 01:11:57 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
 <template>
   <div id="app">
-    <h2>HelloWorld</h2>
-    <i class="far fa-smile-wink"></i>
+    <!-- inizio header -->
+    <header class="py-2">
+      <div class="container container-sm">
+        <div class="row">
+          <div class="col col-sm">
+            <div class="back-nav">
+              <span class="cursor-pointer"><i class="fas fa-arrow-left"></i></span>
+            </div>
+          </div>
+          <div class="col col-sm">
+            <h2 class="text-center sub-menu">Cocktail</h2>
+          </div>
+          <div class="col col-sm">
+            <div class="text-end">
+              <span class="filter cursor-pointer">Filtri</span>
+            </div>
+          </div>
+        </div>
+        <!-- inizio componente di ricerca -->
+        <div class="row mt-2">
+          <div class="col-12">
+            <form>
+              <div class="input-icon">
+                <input class="input-search" type="text" name="search" placeholder="Cerca...">
+                <i class="fas fa-search"></i>
+              </div>
+            </form>
+          </div>
+        </div>
+        <!-- fine componente di ricerca -->
+      </div>
+    </header>
+    <!-- fine header -->
+    <!-- inizio contenuto principale -->
+    <main></main>
+    <!-- fine contenuto principale -->
+    <!-- inizio piè di pagina -->
+    <footer></footer>
+    <!-- fine piè di pagina -->
   </div>
 </template>
 
@@ -17,5 +54,44 @@ export default {
 </script>
 
 <style lang="scss">
+@import '@/css/variables.scss';
 @import '@/css/commons.scss';
+
+// inizio content header
+.filter,
+.back-nav{
+  font-size: 1rem;
+  font-weight: 700;
+  color: $primary-color;
+}
+
+.sub-menu{
+  font-size: 1.3rem;
+  font-weight: 700;
+}
+
+// start campo input di ricerca
+.input-search{
+  width: 100%;
+  padding: 6px 6px 6px 40px;
+  border-radius: 5px;
+  border: 1px solid gray;
+  background: rgb(240, 238, 238);
+}
+
+.input-icon{
+  position: relative;
+
+  & svg{
+    position: absolute;
+    left: 10px;
+    top: 10px;
+    font-size: 1.2rem;
+    color: gray;
+  }
+}
+
+// fine campo input di ricerca
+
+// fine content header
 </style>
